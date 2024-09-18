1022bee5464e03586f5aab4c00c640b970de9af3
minakdr <78243545+minakdr@users.noreply.github.com>
2024-09-18 14:52:35 +0100

    Update SMOTE to handle imbalanced data.R

--- SMOTE to handle imbalanced data.R
+++ SMOTE to handle imbalanced data.R
@@ -1,72 +1,7 @@
-library(dplyr)
-
-data<-read.csv("D:\\Projects\\KaggleV2-May-2016.csv")
-head(data)
-
-
-
-#We remove useless columns
-data <- subset(data , select = -PatientId)
-data <- subset( data , select = -AppointmentID)
-
-#Numerisation of columns 
-
-data$Gender<- as.numeric(factor(data$Gender)) #F=1 , M=2
-data$ScheduledDay <- as.Date(factor(data$ScheduledDay))
-data$AppointmentDay <- as.Date(factor(data$AppointmentDay))
-data$No.show<-as.numeric(factor(data$No.show)) #No=1 , Yes=2
-
-
-# I will create a mapping of the original Neighbourhood names to their numeric values so I need the factors
-neighbourhood_factor <- factor(data$Neighbourhood)
-data$Neighbourhood <- as.numeric(neighbourhood_factor)
-neighbourhood_mapping <- data.frame(
-  Original_Neighbourhood = levels(neighbourhood_factor),
-  Numeric_Value = 1:length(levels(neighbourhood_factor))
-)
-
-print(neighbourhood_mapping)
-
-
-
-#creating a period column ; maybe there is a corelation between the period and no shows 
-
-Calculate_Period <- function(df , col1 , col2) {
-  period <- as.numeric(difftime(df[[col2]], df[[col1]], units = "days"))
-  return(period)}
-data$period <- Calculate_Period( data ,"ScheduledDay", "AppointmentDay")
-data <- subset(data , select = -ScheduledDay)
-data <- subset( data , select = -AppointmentDay)
-head (data)
-
-numerical_columns <- c("Age", "Scholarship", "Hipertension", "Diabetes", "Alcoholism", "Handcap", "SMS_received","period","No.show")
-correlation_matrix1 <- cor(data[, numerical_columns], use = "complete.obs", method = "pearson")  
-print(correlation_matrix1)
-
-
-
-library(reshape2)
-library(ggplot2)
-
-#melt the correlation matrix for ggplot ( make it long format which is more suiable for plotting)
-melted_corr <- melt(correlation_matrix1)
-
-# Plot the heatmap
-ggplot(melted_corr, aes(Var1, Var2, fill = value)) +
-  geom_tile() +
-  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
-                       midpoint = 0, limit = c(-1, 1), space = "Lab", 
-                       name="Correlation") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
-                                   size = 10, hjust = 1)) +
-  coord_fixed()
-
-
-
 
 #Before starting I want to know How many No values we got as the outcome
-d=sum(data$No.show=="2")  # So 22319 peson didn't show up 
+no=sum(data$No.show=="0")# So 87794 person showed up 
+yes=sum(data$No.show=="1")#and 22098 did 
 # the data is imbalanced , therefor we need to use some resampling  techniques to deal wiht this  
 # We will use SMOTE
 
@@ -77,18 +12,18 @@ data$No.show <- as.factor(data$No.show)
 smote_result <- SMOTE(data[, -which(names(data) == "No.show")], 
                       data$No.show, 
                       K = 5, 
-                      dup_size = 44500) 
+                      dup_size = 0) 
 
 # we will combine the results into a balanced dataset
 balanced_data <- smote_result$data
-balanced_data$No.show <- smote_result$class
 
+yesb=sum(balanced_data$class=="1")# the number of no shows increased and now is 66294 
 table(balanced_data$No.show)
 
+#66294 peson didn't show up over 154087 which is way more balanced
+
+
 write.csv(balanced_data, 'SmoteBalencedData.csv', row.names = FALSE)
-dd=sum(balanced_data$class=="2")  # So 66957 peson didn't show up over 1551656 which is way more balanced
-
-
 library(reshape2)
 library(ggplot2)
 
@@ -96,7 +31,7 @@ balanced_data$class <- as.numeric(balanced_data$class)
 
 colnames(balanced_data)[colnames(balanced_data) == "class"] <- "No.show"
 
-correlation_matrix2 <- cor(balanced_data[sapply(balanced_data, is.numeric)])
+correlation_matrix <- cor(balanced_data[sapply(balanced_data, is.numeric)])
 
 
 #melt the correlation matrix for ggplot ( make it long format which is more suiable for plotting)
@@ -112,9 +47,6 @@ ggplot(melted_corr, aes(Var1, Var2, fill = value)) +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, 
                                    size = 10, hjust = 1)) +
   coord_fixed()
-
-
-
 
 
 
